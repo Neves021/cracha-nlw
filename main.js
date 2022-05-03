2f3f04dd3408952eca21469f9768c034cde32893
Evandro Neves de Andrade <103221079+Neves021@users.noreply.github.com>
2022-05-03 17:16:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'Neves021',
+  youtube: 'channel/UC8IBRnhXqmp19dr4qfqlhYA',
+  facebook: 'evandro.neves.52',
+  instagram: 'Neves01_',
+  Twitter: 'Evandro70644381'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
